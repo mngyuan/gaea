1a36d32a9ef43614bb946122547be81a09f912fe
Kevin Lee <me@mngyuan.com>
2021-11-22 14:44:40 -0500

    add threshold shader

--- main.js
+++ main.js
@@ -1,14 +1,32 @@
-let akkuratFont;
+let akkuratFont, thresholdShader, graphicsLayer;
+
+function preload() {
+  akkuratFont = loadFont('assets/Akkurat-Mono.otf');
+  thresholdShader = loadShader(
+    'assets/thresholdShader.vert',
+    'assets/thresholdShader.frag',
+  );
+}
 
 function setup() {
-  createCanvas(1280, 720);
-  akkuratFont = loadFont('assets/Akkurat-Mono.otf');
+  createCanvas(1280, 720, WEBGL);
+  graphicsLayer = createGraphics(1280, 720, WEBGL);
 }
 
 function draw() {
-  background(0);
-  fill(255);
-  textFont(akkuratFont);
-  textSize(24);
-  text('p5*js', 10, 50);
+  const mx = map(mouseX, 0, width, 0, 1);
+  const my = map(mouseY, 0, height, 0, 0.2);
+  shader(thresholdShader);
+  thresholdShader.setUniform('uTexture0', graphicsLayer);
+  thresholdShader.setUniform('uScale', [mx, my]);
+
+  graphicsLayer.background(0);
+
+  graphicsLayer.fill(255);
+  graphicsLayer.textFont(akkuratFont);
+  graphicsLayer.textSize(24);
+  graphicsLayer.text('p5*js', 0, 24);
+
+  // rect gives us some geometry on the screen
+  rect(0, 0, width, height);
 }
