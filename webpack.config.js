8e4a8b958332f6e01760afc77bf1ae6d314468dc
Kevin Lee <me@mngyuan.com>
2021-11-22 13:23:41 -0500

    add typescript

--- webpack.config.js
+++ webpack.config.js
@@ -3,16 +3,28 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   mode: 'development',
+  devtool: 'inline-source-map',
+  devServer: {static: './dist'},
   entry: {
-    index: './src/index.js',
+    index: './src/index.ts',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'GAEA',
+      template: 'src/index.html',
     }),
   ],
-  devtool: 'inline-source-map',
-  devServer: {static: './dist'},
   output: {
     filename: 'main.js',
     path: path.resolve(__dirname, 'dist'),
